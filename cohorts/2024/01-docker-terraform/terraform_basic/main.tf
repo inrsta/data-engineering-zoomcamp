8f888fd0083a50381ae4a48f54e9dbe51a6e427f
Inri Staka <inri.staka@gmail.com>
2024-01-28 00:01:30 +0100

    finished seventh question

---
+++ cohorts/2024/01-docker-terraform/terraform_basic/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+# Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
+#  credentials = 
+  project = "dataeng2024"
+  region  = "eu-west1"
+}
+
+
+
+resource "google_storage_bucket" "data-lake-bucket" {
+  name          = "dataeng2024"
+  location      = "EU"
+
+  # Optional, but recommended settings:
+  storage_class = "STANDARD"
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled     = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30  // days
+    }
+  }
+
+  force_destroy = true
+}
+
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id = "taxi_dataset"
+  project    = "dataeng2024"
+  location   = "EU"
+}
